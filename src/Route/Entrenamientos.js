ed9e107549b9d8388a7a4903ab12bfd24c0bdaf0
MiguelVegaM <miguelvegam200@gmail.com>
2023-06-21 09:43:21 -0600

    capacitacion almost done

--- src/Route/Entrenamientos.js
+++ src/Route/Entrenamientos.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useDebugValue } from "react";
 
 import { Helmet } from "react-helmet";
 
@@ -18,55 +18,67 @@ import "../styles/Entrenamientos/Capacitacion.css";
 
 export function EntrenamientosHeader() {
   return (
-    <div className="capacitacion">
-      <h1 className="entrenamientos__header">CAPACITACIÓN</h1>
-      <p className="entrenamientos__paragraph">
-        Fielmente comprometidos con la predicación de evangelio de Jesucristo,
-        entrenando a expositores bíblicos en el mundo hispano parlante.
-      </p>
-      <Button specificClass="btn__capacitacion-main-banner">CONTÁCTANOS</Button>
+    <div className="capacitacion container-fluid">
+      <div className="capacitacion-container">
+        <h1 className="entrenamientos__header">CAPACITACIÓN</h1>
+        <p className="entrenamientos__paragraph">
+          Fielmente comprometidos con la predicación de evangelio de Jesucristo,
+          entrenando a expositores bíblicos en el mundo hispano parlante.
+        </p>
+        <Button specificClass="btn__capacitacion-main-banner">
+          CONTÁCTANOS
+        </Button>
+      </div>
     </div>
   );
 }
 
+// ! quitar margenes en media querie 700px <----------
+
 export function EntrenamientosEntrenamientosOnline() {
   return (
-    <div className="entrenamientos__entrenamientos-online">
-      <img
-        className="entrenamientos-entrenamiento__card-image"
-        src="/images/entrenamientos/estudiante-sonriente-tiro-medio-estudiando-laptop.jpg"
-        alt="Men on laptop"
-      />
-
-      <div className="entrenamientos-entrenamiento__card-text">
-        <h1 className="entrenamiento__card-title">Entrenamientos</h1>
-        <div className="entrenamiento__modalidad">
-          Modalidad:{" "}
-          <span className="entrenamiento__card-text-bold"> En Línea</span>
-          <span className="online-dot">.</span>
+    <div className="container-fluid-xl">
+      <div className="entrenamientos__entrenamientos-online row">
+        <div className="col-12 col-md-6 no padding">
+          <img
+            className="entrenamientos-entrenamiento__card-image"
+            src="/images/entrenamientos/estudiante-sonriente-tiro-medio-estudiando-laptop.jpg"
+            alt="Men on laptop"
+          />
         </div>
-        <p className="entrenamiento__card-text-bold">
-          Debido a las limitaciones de movilidad y el distanciamiento social que
-          hemos estado experimentando en el mundo en los últimos meses, Prédica
-          Fiel ha diseñado un entrenamiento online para predicadores con el
-          objetivo de desarrollar las habilidades de los mismos semana tras
-          semana.
-        </p>
-        <p className="entrenamiento__card-text-normal">
-          Hemos escogido el género literario de Evangelios para este primer
-          tiempo, especificamente el evangelio de Marcos. Los participantes
-          estarán recibiendo instrucciones en el manejo adecuado de las
-          Escrituras, así como en la exposición de estas. Trabajarán pasajes
-          específicos del libro de Marcos, así como también harán una exposición
-          de veinte minutos, para las cuáles recibirán retroalimentación.
-        </p>
-        <div className="entrenamientos-entrenamiento__button-container-box">
-          <Button specificClass="btn__entrenamientos-entrenamientos-online--rojo">
-            ACCESO A RECURSOS
-          </Button>
-          <Button specificClass="btn__entrenamientos-entrenamientos-online--gris">
-            APLICAR AHORA
-          </Button>
+        <div className="col-12 col-md-6 nopadding">
+          <div className="entrenamientos-entrenamiento__card-text">
+            <h1 className="entrenamiento__card-title">Entrenamientos</h1>
+            <div className="entrenamiento__modalidad">
+              Modalidad:{" "}
+              <span className="entrenamiento__card-text-bold"> En Línea</span>
+              <span className="online-dot">.</span>
+            </div>
+            <p className="entrenamiento__card-text-bold">
+              Debido a las limitaciones de movilidad y el distanciamiento social
+              que hemos estado experimentando en el mundo en los últimos meses,
+              Prédica Fiel ha diseñado un entrenamiento online para predicadores
+              con el objetivo de desarrollar las habilidades de los mismos
+              semana tras semana.
+            </p>
+            <p className="entrenamiento__card-text-normal">
+              Hemos escogido el género literario de Evangelios para este primer
+              tiempo, especificamente el evangelio de Marcos. Los participantes
+              estarán recibiendo instrucciones en el manejo adecuado de las
+              Escrituras, así como en la exposición de estas. Trabajarán pasajes
+              específicos del libro de Marcos, así como también harán una
+              exposición de veinte minutos, para las cuáles recibirán
+              retroalimentación.
+            </p>
+            <div className="entrenamientos-entrenamiento__button-container-box">
+              <Button specificClass="btn__entrenamientos-entrenamientos-online--rojo">
+                ACCESO A RECURSOS
+              </Button>
+              <Button specificClass="btn__entrenamientos-entrenamientos-online--gris">
+                APLICAR AHORA
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -76,33 +88,36 @@ export function EntrenamientosEntrenamientosOnline() {
 export function PlanHabana() {
   return (
     <div className="plan-habana">
-      <div className="plan-habana-banner">
-        <div className="plan-habana-banner__text">
-          <h1 className="plan-habana-banner__header">PLAN HABANA</h1>
-          <p className="modalidad">
-            Modalidad:{" "}
-            <span className="plan-habana-banner--bold">Presencial,</span> Cuba.
-          </p>
-          <p className="plan-habana-banner--bold">
-            Plan Habana nace de la pasión de transformar <br></br>
-            La Habana con el Evangelio de Jesucristo
-          </p>
-          <p className="plan-habana-banner--small">
-            pero como dicen Las Escrituras: "¿Cómo, pues, invocarán a aquel en
-            quien no han creído? ¿Y cómo creerán en aquel de quien no han oído?
-            ¿Y cómo oirán sin haber quien les predique? (Romanos 10:14).{" "}
-            <span className="plan-habana-banner--bold">
-              A través de Plan Habana, capacitamos a pastores, maestros y
-              líderes en cómo predicar el evangelio fielmente.
-            </span>
-          </p>
-        </div>
-        <div className="plan-habana-banner__imagen-container">
-          <img
-            className="plan-habana-banner__imagen"
-            src="/images/entrenamientos/entrenamientoplanhabana.jpg"
-            alt="Grupo Estudiando"
-          />
+      <div className="plan-habana-banner container-fluid">
+        <div className="row">
+          <div className="plan-habana-banner__text col-lg-6">
+            <h1 className="plan-habana-banner__header">PLAN HABANA</h1>
+            <p className="modalidad">
+              Modalidad:{" "}
+              <span className="plan-habana-banner--bold">Presencial,</span>{" "}
+              Cuba.
+            </p>
+            <p className="plan-habana-banner--bold">
+              Plan Habana nace de la pasión de transformar <br></br>
+              La Habana con el Evangelio de Jesucristo
+            </p>
+            <p className="plan-habana-banner--small">
+              pero como dicen Las Escrituras: "¿Cómo, pues, invocarán a aquel en
+              quien no han creído? ¿Y cómo creerán en aquel de quien no han
+              oído? ¿Y cómo oirán sin haber quien les predique? (Romanos 10:14).{" "}
+              <span className="plan-habana-banner--bold">
+                A través de Plan Habana, capacitamos a pastores, maestros y
+                líderes en cómo predicar el evangelio fielmente.
+              </span>
+            </p>
+          </div>
+          <div className="plan-habana-banner__imagen-container  col-lg-6 p-0">
+            <img
+              className="plan-habana-banner__imagen img-responsive"
+              src="/images/entrenamientos/entrenamientoplanhabana.jpg"
+              alt="Grupo Estudiando"
+            />
+          </div>
         </div>
       </div>
       <div className="plan-habana-exposicion">
@@ -215,21 +230,25 @@ export function Internado() {
             Plan Habana, Modalidad Presencial, Cuba
           </div>
         </div>
-        <div className="internado__images-container">
-          <img
-            className="internado__image"
-            src="/images/entrenamientos/banner10.jpg"
-            alt=""
-          />
-          <img
-            className="internado__image"
-            src="/images/entrenamientos/Foto.jpg"
-            alt=""
-          />
+        <div className="internado__images-container row">
+          <div className="col-6">
+            <img
+              className="internado__image"
+              src="/images/entrenamientos/banner10.jpg"
+              alt=""
+            />
+          </div>
+          <div className="col-6">
+            <img
+              className="internado__image"
+              src="/images/entrenamientos/Foto.jpg"
+              alt=""
+            />
+          </div>
         </div>
       </div>
-      <div className="talleres">
-        <div className="talleres__text">
+      <div className="talleres row">
+        <div className="talleres__text col-12 col-lg-8 nopadding">
           <div className="talleres__header">TALLERES</div>
           <div className="talleres__paragraph">
             El propósito de los talleres es ayudar a los participantes a crecer
@@ -240,11 +259,14 @@ export function Internado() {
           </div>
           <Button specificClass="btn__talleres">MÁS INFORMACIÓN</Button>
         </div>
-        <img
-          className="talleres__image"
-          src="/images/entrenamientos/ponente-escenario-frente-sala-vista-posterior-audiencia-accion-puesta.png"
-          alt=""
-        />
+
+        <div className="col-12 col-lg-4 nopadding">
+          <img
+            className="talleres__image"
+            src="/images/entrenamientos/ponente-escenario-frente-sala-vista-posterior-audiencia-accion-puesta.png"
+            alt=""
+          />
+        </div>
       </div>
     </div>
   );
