580822fc19bcd3f17876574e2a3f286311b3d603
MiguelVegaM <miguelvegam200@gmail.com>
2023-10-13 13:20:03 -0600

    minor tweaks 2

--- src/Route/Inicio.js
+++ src/Route/Inicio.js
@@ -539,26 +539,26 @@ export function Testimonios(props) {
   );
 }
 
-export function Cobime(props) {
-  return (
-    <div className="cobime">
-      <h5 className="cobime__header">
-        Conferencia Bíblica para Minustros Evangelicos
-      </h5>
-      <h1 className="cobime__main">
-        PREPARADOS PARA <span className="cobime__main--bold">COBIME 2024</span>
-      </h1>
-      <h4 className="cobime__paragraph">
-        preparate para este tiempo de aprendizaje
-      </h4>
-      <div className="cobime__button-container">
-        <a href="#contacto">
-          <Button>ANOTARME</Button>
-        </a>
-      </div>
-    </div>
-  );
-}
+// export function Cobime(props) {
+//   return (
+//     <div className="cobime">
+//       <h5 className="cobime__header">
+//         Conferencia Bíblica para Minustros Evangelicos
+//       </h5>
+//       <h1 className="cobime__main">
+//         PREPARADOS PARA <span className="cobime__main--bold">COBIME 2024</span>
+//       </h1>
+//       <h4 className="cobime__paragraph">
+//         preparate para este tiempo de aprendizaje
+//       </h4>
+//       <div className="cobime__button-container">
+//         <a href="#contacto">
+//           <Button>ANOTARME</Button>
+//         </a>
+//       </div>
+//     </div>
+//   );
+// }
 
 export function Alianzas(props) {
   return (
